b1628bd73e5f548ef4352b089435527982dada91
sfriedenberg-etsy <sfriedenberg@etsy.com>
2026-06-25 11:20:34 -0400

    bump nixpkgs-master: d233902 → 567a49d

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     # nixpkgs) is required — re-applying gomod2nix's upstream overlay would
     # shadow the patched builder. Matches tommy/moxy/maneater.
     igloo.url = "github:amarbel-llc/igloo";
-    nixpkgs-master.url = "github:NixOS/nixpkgs/d233902339c02a9c334e7e593de68855ad26c4cb";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/567a49d1913ce81ac6e9582e3553dd90a955875f";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
 
     # trapeze's go.mod requires a newer Go patch than the eng-pinned
