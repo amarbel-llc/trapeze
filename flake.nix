5bfaa7251b1069210f2586f6d168e248fdf80e8a
friedenberg <1baggzpr@starbrandshoes.com>
2026-09-08 19:44:44 +0000

    bump nixpkgs-master: 567a49d → f13ff45
    conformist: 3ada15e → 7ed393c
    igloo: 20e7e8e → b13d154
    nixpkgs-master: 567a49d → f13ff45
    treefmt-nix: ae79109 → 27b3b12

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     igloo.url = "https://code.linenisgreat.com/igloo/archive/master.tar.gz";
     igloo.inputs.nixpkgs-master.follows = "nixpkgs-master";
     igloo.inputs.treefmt-nix.follows = "treefmt-nix";
-    nixpkgs-master.url = "github:NixOS/nixpkgs/567a49d1913ce81ac6e9582e3553dd90a955875f";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/f13ff45afd1bb73e640eaa08a7066dbed07e3238";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
     utils.inputs.systems.follows = "igloo/systems";
 
